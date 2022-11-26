a1f99868d21babd9ee383982c6dc43efc1117ba3
Abnatal <abnatal@gmail.com>
2022-11-26 11:06:39 -0300

    feat: make post title and content required (not empty)

---
+++ railsblog/db/migrate/20221126135842_make_post_title_and_content_not_null.rb
@@ -0,0 +1,6 @@
+class MakePostTitleAndContentNotNull < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :posts, :title, false
+    change_column_null :posts, :content, false
+  end
+end
--- railsblog/db/schema.rb
+++ railsblog/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_133324) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_135842) do
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
