2806296e95c3cf8bf53fe8520962d938db15f832
Abnatal <abnatal@gmail.com>
2022-11-27 12:25:05 -0300

    feat: added references from User to Posts and Comments.

--- railsblog/app/models/post.rb
+++ railsblog/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :user
     validates_presence_of :title, :content
     has_rich_text :content
     has_many :comments, dependent: :destroy
--- railsblog/config/routes.rb
+++ railsblog/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  
+
+  devise_for :users  
   root "posts#index"
 end
