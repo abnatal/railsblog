2f5e11917b26a418128c7690cb4aece739b5939f
Abnatal <abnatal@gmail.com>
2022-11-27 12:52:01 -0300

    fix: added user_id on create method of CommentsController

--- railsblog/app/controllers/comments_controller.rb
+++ railsblog/app/controllers/comments_controller.rb
@@ -2,12 +2,17 @@ class CommentsController < ApplicationController
     before_action :set_post
 
     def create
-        comment = @post.comments.create! params.required(:comment).permit(:content)
+        comment = @post.comments.create(comment_params.to_h.merge!({ user_id: current_user.id }))
         CommentsMailer.submitted(comment).deliver_later
         redirect_to @post
     end
 
-    private def set_post
+    private
+    def set_post
         @post = Post.find(params[:post_id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content)
     end
 end
