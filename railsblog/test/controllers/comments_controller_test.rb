421c70a083c5e16731a8fe2628a1b10ceddeeb60
Abnatal <abnatal@gmail.com>
2022-11-28 18:16:42 -0300

    test: added tests to CommentsControllerTest: users should not delete comments made by another user

--- railsblog/test/controllers/comments_controller_test.rb
+++ railsblog/test/controllers/comments_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @post_one = posts(:one)
+    @post_two = posts(:two)
+    @comment_one = comments(:one)
+    @comment_two = comments(:two)
+  end
+
+  test "should destroy comment" do
+    sign_in users(:one)
+
+    assert_difference("Comment.count", -1) do
+      delete post_comment_path(@post_one, @comment_one)
+    end
+
+    assert_redirected_to post_url(@post_one)
+  end
+
+  test "should NOT destroy another user comment" do
+    sign_in users(:one)
+
+    assert_difference("Comment.count", 0) do
+      delete post_comment_path(@post_two, @comment_two)
+    end
+
+    assert_redirected_to root_path
+  end
+
 end
